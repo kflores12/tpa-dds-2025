3d45d97d226d1650ae3380d1f6c37df116f6ca6a
Kevin <kfloresportillo@frba.utn.edu.ar>
2025-05-05 18:09:18 -0300

    Actualizacion contribuyente

--- src/main/java/ar/edu/utn/frba/dds/dominio/Contribuyente.java
+++ src/main/java/ar/edu/utn/frba/dds/dominio/Contribuyente.java
@@ -5,5 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Contribuyente extends Visualizador {
+  private String nombre;
+  private String apellido;
+  private Integer edad;
+
+  public Contribuyente() {
+    //para contribuyente anonimo
+  }
+
+  public Contribuyente(String nombre, String apellido, Integer edad) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+  }
+
+  public boolean esContribuyenteAnonimo() {
+    return nombre == null && apellido == null && edad == null;
 
 }
