19a4e8c772f2136bf1d03ec8c4d16dc5bb1daec9
Kevin <kfloresportillo@frba.utn.edu.ar>
2025-07-04 18:50:47 -0300

    Update Main.java

--- src/main/java/ar/edu/utn/frba/dds/dominio/Main.java
+++ src/main/java/ar/edu/utn/frba/dds/dominio/Main.java
@@ -1,47 +1,66 @@
 package ar.edu.utn.frba.dds.dominio;
 
 import java.net.URL;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.time.LocalDate;
 
+
+//No esta terminado todavia
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("INICIO: Ejecución iniciada a las " + LocalDateTime.now());
-    try {
-      Hecho hecho = new Hecho("Corte de luz Dinamica",
-          "Corte de luz en zona sur", "cortes", 21.2,
-          12.8, LocalDate.of(2025, 1, 1),
-          LocalDate.now(), TipoFuente.DINAMICA, "", Boolean.TRUE);
+    public static void main(String[] args) {
+        System.out.println("INICIO: Ejecución iniciada a las " + LocalDateTime.now());
+        try {
+          
+          FuenteDataSet fuenteDataSet = new FuenteDataSet("/home/spinozista/tpa-2025-05/EjHechos.csv","d/M/yyyy",';');
+          RepositorioHechos repositorioDeHechos = new RepositorioHechos();
+          RepositorioHechos repositorioDeHechos2 = new RepositorioHechos();
+          SolicitudDeCarga solicitudDeCargaPrimera = new SolicitudDeCarga("Corte de luz","Corte de luz en zona sur"
+          ,"cortes",21.2,12.8, LocalDate.of(2025,1,1),"",Boolean.TRUE,repositorioDeHechos2);
+          solicitudDeCargaPrimera.aprobar();
+          FuenteDinamica fuenteDinamica = new FuenteDinamica(repositorioDeHechos2);
+          RepositorioFuentes repositorioDeFuentes = new RepositorioFuentes();
+          ConexionMock conexion = new ConexionMock();
+          URL url = new URL("http://mock.url");
+          FuenteProxyDemo fuenteProxy = new FuenteProxyDemo(conexion, url, repositorioDeHechos);
+          fuenteProxy.obtenerHechos();
+          repositorioDeFuentes.registrarFuente(fuenteDinamica);
+          repositorioDeFuentes.registrarFuente(fuenteDataSet);
+          repositorioDeFuentes.registrarFuente(fuenteProxy);
+
+           if ("consensuarHechos".equals(args[0])) {
+                consensuarHechos(repositorioDeFuentes);
+            } else if ("ejecutarAgregador".equals(args[0])) {
+                ejecutarAgregador(repositorioDeFuentes);
+            }
 
 
-      RepositorioHechos repositorioDeHechos = new RepositorioHechos();
-      RepositorioHechos repositorioDeHechos2 = new RepositorioHechos();
-      repositorioDeHechos2.cargarHecho(hecho);
-      FuenteDinamica fuenteDinamica = new FuenteDinamica(repositorioDeHechos2);
-      RepositorioFuentes repositorioDeFuentes = new RepositorioFuentes();
-      ConexionMock conexion = new ConexionMock();
-      URL url = new URL("http://mock.url");
-      FuenteProxyDemo fuenteProxy = new FuenteProxyDemo(conexion, url, repositorioDeHechos);
-      fuenteProxy.obtenerHechos();
-      FuenteDataSet fuenteDataSet = new FuenteDataSet("EjHechos.csv", "d/M/yyyy",
-          ';');
-      repositorioDeFuentes.registrarFuente(fuenteDinamica);
-      repositorioDeFuentes.registrarFuente(fuenteDataSet);
-      repositorioDeFuentes.registrarFuente(fuenteProxy);
-      FiltroBaseAgregador filtroBase = new FiltroBaseAgregador();
-      Agregador agregador = new Agregador(repositorioDeFuentes, filtroBase);
-      agregador.agregarHechos();
-      List<Hecho> hechos = agregador.getHechos();
-      System.out.println("Cantidad de hechos: " + hechos.size());
-
-    } catch (Exception e) {
-      System.err.println("ERROR: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("ERROR: " + e.getMessage());
+        } 
+            System.out.println("FIN: Ejecución finalizada a las " + LocalDateTime.now());
     }
-    System.out.println("FIN: Ejecución finalizada a las " + LocalDateTime.now());
 
-  }
+    private static void consensuarHechos(RepositorioFuentes repositorioDeFuentes) throws Exception {
+        FiltroBaseAgregador filtroBase = new FiltroBaseAgregador();
+          Agregador agregador = new Agregador(repositorioDeFuentes, filtroBase);
+          agregador.agregarHechos();
+        List<Hecho> hechos = agregador.getHechos();
+        FuenteDataSet fuenteDataSet = new FuenteDataSet("/home/spinozista/tpa-2025-05/EjHechos.csv","d/M/yyyy",';');
+        Coleccion coleccion = new Coleccion("coleccionPrueba","coleccionPrueba",fuenteDataSet, List.of(),"1",new AmayoriaSimple());
+        coleccion.actualizarHechosConsensuados(hechos); 
+        
+    } 
+
+    private static void ejecutarAgregador(RepositorioFuentes repositorioDeFuentes) throws Exception {
+        FiltroBaseAgregador filtroBase = new FiltroBaseAgregador();
+          Agregador agregador = new Agregador(repositorioDeFuentes, filtroBase);
+          agregador.agregarHechos();
+        List<Hecho> hechos = agregador.getHechos();
+        System.out.println("Cantidad de hechos: " + hechos.size());
+        
+
+    }
 }
-
 
 
